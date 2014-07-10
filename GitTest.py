7c4f846113e5a03b378b457a565ce34ceb383841
RSilverFitzmall <silverr@fitzgeraldautomall>
2014-07-10 13:15:03 -0400

    Testing the pre-commit hook

--- GitTest.py
+++ GitTest.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 from subprocess import Popen, PIPE, call
+
+
 origin = 'C:\SourceControl\Test.git'
 branch = 'master'
 
@@ -13,16 +15,32 @@ arg3 = ['git','status']
 
 arg4 = ['git','push', origin, branch]
 
+arg5 = ['touch', 'deleteme.txt']
+
 args = [arg1,arg2,arg2ish,arg3,arg4]
 
-def SysCommand(args):
-	#kwargs.setdefault('stdout', PIPE)
-	proc = Popen(args)#,stdout=PIPE,stderr=PIPE)#, kwargs)
+'''
+def SysCommand(args,**kwargs):
+	kwargs.setdefault('stdout', PIPE)
+	proc = Popen(args,**kwargs)#,stdout=PIPE,stderr=PIPE)#, kwargs)
 	out, err = proc.communicate()
 	proc.wait()
 	code = proc.returncode
 	return out
+''' 
+class SysCommand:
+	def __init__(self,args,**kwargs):
+		self.args = args
+		kwargs.setdefault('stdout',PIPE)
+		proc = Popen(args,**kwargs)
+		self.out, self.err = proc.communicate()
+		proc.wait()
+		self.code = proc.returncode
 
+
+print(SysCommand(['git','commit']).out)
 
 for a in args:
-	print(SysCommand(a))
+	print(SysCommand(a).out)
+
+print(SysCommand(arg5,cwd='InnerTest').out)
