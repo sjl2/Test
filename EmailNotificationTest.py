cfea587369eed92ae3993effd0b1cbb2660d6562
RSilverFitzmall <silverr@fitzgeraldautomall>
2014-07-29 13:39:06 -0400

    Testing pre-commit hook

--- EmailNotificationTest.py
+++ EmailNotificationTest.py
@@ -9,8 +9,8 @@ from email.mime.multipart import MIMEMultipart
 
 developer_email = 'stewart_lynch@brown.edu'
 sender ='lynchs@fitzgeraldautomall.com'
-subject = "DO NOT REPLY: " + "Hello"
-body = "HOPEFULLY THIS MESSAGE FINDS YOU IN GOOD AUTOMATED HEALTH MY FELLOW COMPUTER"
+subject = "YOU WANT TO BUY A CAR"
+body = "NEED A CAR??\nCOME TO FIZGERALD AUTOMALL TODAY!"
 
 message = MIMEMultipart()
 message['From'] = developer_email
@@ -21,11 +21,12 @@ message.attach(MIMEText(body,'plain'))
 
 try:
 	smtpObj = smtplib.SMTP("smtp.emailsrvr.com", 587)
-	smtpObj.set_debuglevel(True)  # show communication with the server
+	#smtpObj.set_debuglevel(True)  # show communication with the server
 	smtpObj.ehlo_or_helo_if_needed()
 	smtpObj.starttls()
 	smtpObj.login(sender,"sh0rtt1m3")
-	smtpObj.sendmail(sender, developer_email, message.as_string())
+	for i in range(0,10):
+		smtpObj.sendmail(sender, developer_email, message.as_string())
 	print("Successfully sent email.")
 	smtpObj.quit()
 except smtplib.SMTPHeloError:
@@ -33,10 +34,6 @@ except smtplib.SMTPHeloError:
 except smtplib.SMTPException:
 	print("Error: unable to send mail")
 	print(smtplib.SMTPException.strerror)
-
-
-
-
 """
 try:
 	smtpObj = smtplib.SMTP("secure.emailsrvr.com", 465)
